882085ccc87c69238904c79fc7371acd64a21b51
Okan Fidan <okanfidan5077@gmail.com>
2020-08-25 17:32:31 +0300

    web api transfer action command, event,microservice checkin

---
+++ MicroRabbit/MicroRabbit.Banking.Application/Models/AccountTransfer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Application.Models
+{
+    public class AccountTransfer
+    {
+        public int SourceAccount { get; set; }
+        public int TargetAccount { get; set; }
+        public decimal TransferAmounth { get; set; }
+    }
+}
--- MicroRabbit/MicroRabbit.Banking.Application/Services/AccountService.cs
+++ MicroRabbit/MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -1,6 +1,9 @@
 ﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Banking.Domain.Models;
+using MicroRabbit.Domain.Core.Bus;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,15 +12,27 @@ namespace MicroRabbit.Banking.Application.Services
 {
     public class AccountService : IAccountService
     {
+        private readonly IEventBus _bus;
         private readonly IAccountRepository _accountRepository;
 
-        public AccountService(IAccountRepository accountRepository)
+        public AccountService(IAccountRepository accountRepository, IEventBus bus)
         {
             _accountRepository = accountRepository;
+            _bus = bus;
         }
         public IEnumerable<Account> GetAccounts()
         {
             return _accountRepository.GetAccounts();
+        }
+
+        public void Transfer(AccountTransfer accountTransfer)
+        {
+            var createTransferCommand = new CreateTransferCommand(
+                accountTransfer.SourceAccount,
+                accountTransfer.TargetAccount,
+                accountTransfer.TransferAmounth);
+
+            _bus.SendCommand(createTransferCommand);
         }
     }
 }
